3c03f96d7dc2132285851d07d2e943c88bd29596
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2020-09-30 15:09:34 +0300

    docs: task done human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function toReadable (number) {
-  
-}
+
+    let array1_19 = [ '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let array20_90 = [ '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety' ];
+
+
+  let num = number.toString();
+  let res = '';
+  num = num.replace(/[\, ]/g,'');
+       if(number === 0 ){
+         res = 'zero';
+     }
+  else if(number < 20){
+    res = array1_19[number];
+  }else if(num.length < 3){
+    res =  array20_90[num[0]] + ' ' + array1_19[num[1]];
+  }else if(num.length < 4 ){
+     if( num[1] !== '1'){
+      res = array1_19[num[0]] + ' hundred ' + array20_90[num[1]] + ' ' + array1_19[num[2]];
+     }else{
+      res =  array1_19[num[0]] + ' hundred ' + array1_19[num[1] + num[2]];
+     } 
+  }
+  res = res.replace(/ +/g, ' ').trim();
+  return res;  
+  };
